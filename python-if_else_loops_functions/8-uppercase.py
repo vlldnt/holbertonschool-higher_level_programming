82d1f44d53bced37243417d290aecd03f79e7851
vlldnt <vieilledent.adrien@gmail.com>
2025-01-14 10:58:58 +0100

    print in upper case the lower cases letter

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-def islower(c):
-    if ord(c) < 97 and ord(c) > 122:
-        return False
-    else:
-        return True
+def uppercase(str):
+    upper = ""
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            upper = upper + chr(ord(c) - 32)
+        else:
+            upper = upper + c
+    print(f"{upper}")
