5e6bb00013d93cf8561c1a757343182af09e83a0
vlldnt <vieilledent.adrien@gmail.com>
2025-01-15 10:00:42 +0100

    pycodestyle task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-            print("{:d}{:d}".format(i, j), end="\n" if i == 8 and j == 9 else ", ")
+        print("{:d}{:d}".format(i, j), end="\n" if i == 8 and j == 9 else ", ")
