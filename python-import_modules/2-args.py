c552ed21e9917d38de903876f6ef0a15350029d1
vlldnt <vieilledent.adrien@gmail.com>
2025-01-15 16:53:11 +0100

    Task 2 corrected on number of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif len == 2:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len))
+        print("{} arguments:".format(len - 1))
     for n in range(1, len):
         print("{}: {}".format(n, argv[n]))
