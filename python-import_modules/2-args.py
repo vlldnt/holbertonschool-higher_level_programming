d7b2e948d588209466c6ccd710a78f73a6dd65de
vlldnt <vieilledent.adrien@gmail.com>
2025-01-15 16:49:54 +0100

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-len = len(argv)
-if len == 1:
-    print("0 arguments.")
-elif len == 2:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(len))
-for n in range(1, len):
-    print("{}: {}".format(n, argv[n]))
+if __name__ == "__main__":
+    len = len(argv)
+    if len == 1:
+        print("0 arguments.")
+    elif len == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len))
+    for n in range(1, len):
+        print("{}: {}".format(n, argv[n]))
