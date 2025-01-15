6ecbe9fb15164029c53401de1e25bb725067ba5d
vlldnt <vieilledent.adrien@gmail.com>
2025-01-15 09:41:47 +0100

    Task 9 print mast digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
-    last = ""
-    if number < 0:
-        last = number % 10 * -1
-    else:
-        last = number % 10
+    last = abs(number) % 10
+    print(last, end="")
     return last
