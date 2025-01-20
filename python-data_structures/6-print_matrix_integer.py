2c60f0bd01781063b73c24fe109d9ef4d15e5fa9
vlldnt <vieilledent.adrien@gmail.com>
2025-01-20 12:04:41 +0100

    Task 6 added and 5 corrected

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, element in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(element), end=" ")
+            else:
+                print("{:d}".format(element), end="")
+        print()
