f995df19e6588279c036a8d28c4a2c38e12ed904
vlldnt <vieilledent.adrien@gmail.com>
2025-01-20 12:10:18 +0100

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    if length == 0:
-        return None
+    if sentence == "":
+        return 0, None
     else:
-        return length, first
+        return len(sentence), sentence[0]
