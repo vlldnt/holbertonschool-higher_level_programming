8da4c092c3db7c9d7177163aa9630d0f6e5eb1a4
vlldnt <vieilledent.adrien@gmail.com>
2025-01-21 14:06:52 +0100

    correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    print("{}".format(my_list[x]))
+    i = 0
+    while i < x:
+        print("{:d}".format(my_list[i]), end="\n" if i == x - 1 else "")
+        i += 1
+    if x > len(my_list):
+        return len(my_list)
+    return x
--- python_exceptions/0-safe_print_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    print("{}".format(my_list[x]))
