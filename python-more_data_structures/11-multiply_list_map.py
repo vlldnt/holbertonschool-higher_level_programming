2f530a51950557c8dbd523584faa51488d8e6185
vlldnt <vieilledent.adrien@gmail.com>
2025-01-21 10:43:48 +0100

    Task 10 11 12

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda num_list: num_list * number, my_list))
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    
+    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+
+    for char in roman_string:
+        if char not in roman_numbers:
+            return 0
+        else:
+            value = roman_numbers[char]
+            result += value
+    return result
 
