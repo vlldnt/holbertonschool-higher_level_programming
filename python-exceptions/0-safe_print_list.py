92adb691041084a85ac6fcc14d1ac590621f8aec
vlldnt <vieilledent.adrien@gmail.com>
2025-01-21 15:03:58 +0100

    Task 0 adn 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    while i < x:
-        print("{:d}".format(my_list[i]), end="\n" if i == x - 1 else "")
-        i += 1
-    if x > len(my_list):
-        return len(my_list)
-    return x
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            i += 1
+        print("")
+    except IndexError:
+        print("")
+    return i
