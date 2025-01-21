1574b86a060c1dab7096a6bfc18e6203e3105cc9
vlldnt <vieilledent.adrien@gmail.com>
2025-01-21 11:02:39 +0100

    Task 1à and 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,11 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    
-    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    roman_numbers = {
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
     result = 0
 
     for char in roman_string:
@@ -13,4 +16,3 @@ def roman_to_int(roman_string):
             value = roman_numbers[char]
             result += value
     return result
-
