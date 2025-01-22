5a77e8434fe8656748b1f0c15d8853415815514f
vlldnt <vieilledent.adrien@gmail.com>
2025-01-22 09:50:44 +0100

    Task 4

--- python-exceptions/4-main.py
+++ python-exceptions/4-main.py
@@ -8,7 +8,7 @@ print(result)
 
 print("--")
 
-my_l_1 = [10, 8, 4, 4]
+my_l_1 = [10, 8, 4, 4, 7]
 my_l_2 = [2, 0, "H", 2, 7]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
 print(result)
