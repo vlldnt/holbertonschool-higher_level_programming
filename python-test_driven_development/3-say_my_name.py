cd80c07619782713060d12985183f8638c5ea8e2
vlldnt <vieilledent.adrien@gmail.com>
2025-01-23 14:01:39 +0100

    Task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 '''
 Print a string with first name with last name
+The first name and last name
+print the full name
 '''
+
+
 def say_my_name(first_name, last_name=""):
+    '''Function that prints name and last name'''
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
