49461c323a378cae66f9c36d08e343d91178371d
vlldnt <vieilledent.adrien@gmail.com>
2025-01-23 09:45:44 +0100

    space correction

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,8 +16,8 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix"
-                        "(list of lists) of integers/floats")
+        raise TypeError("matrix must be a"
+                        "matrix(list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
