1b816be071ff572d2a2fe73519fa0f729f486472
vlldnt <vieilledent.adrien@gmail.com>
2025-01-24 11:03:21 +0100

    Task 6 updated with better name

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,11 +11,21 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         '''First tests unittest '''
         self.assertEqual(max_integer([]), None)
+
+    def test_regular_max(self):
+        '''Regular tests'''
         self.assertEqual(max_integer([2, 2, 2]), 2)
         self.assertEqual(max_integer([51]), 51)
         self.assertEqual(max_integer([51, -876, 987]), 987)
+
+    def test_one_inf_zero_ony(self):
+        '''integer < 0 test'''
         self.assertEqual(max_integer([-51]), -51)
+
+    def test_none(self):
+        '''None test'''
         self.assertEqual(max_integer([None]), None)
-        self.assertEqual(max_integer(['i', 'o']), 'o')
-        self.assertEqual(max_integer(['zephir', 'Zephir', 'zéphir']), 'zéphir')
+    
+    def test_edge_case(self):
+        '''infinity test'''
         self.assertEqual(max_integer([float('inf'), 8, -987]), float('inf'))
