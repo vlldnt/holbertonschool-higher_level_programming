c437f9e031bcb589f4c2b33e1806816434f94a7c
vlldnt <vieilledent.adrien@gmail.com>
2025-01-27 09:46:20 +0100

    Task 1- privtae instatiation

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Creation of an empty class: Square
+'''
+
+
+class Square:
+    '''Empty classe named Square'''
+
+    def __init__(self, size):
+        '''istantiation with size in private'''
+        self.__size = size
