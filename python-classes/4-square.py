977316fb06172d0f2a61361e33d1965856604132
vlldnt <vieilledent.adrien@gmail.com>
2025-01-27 10:33:08 +0100

    Task 4 - Getter and setter size

---
+++ python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Creation of an empty class: Square
+'''
+
+
+class Square:
+    '''Empty classe named Square'''
+
+    def __init__(self, size=0):
+        '''Private istantiation with size'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        ''' Calcul of the area of the square
+            Public instantiation of area
+        '''
+        area = self.__size * self.__size
+        return area
+
+    @property
+    def size(self):
+        '''Getter to retrieve the size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Property setter  to give value to  __size'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        return self.__size
