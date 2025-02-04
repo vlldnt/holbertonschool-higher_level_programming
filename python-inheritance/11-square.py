7f8a037a43a28bc8ede8e4e9e7caf1af35f45b10
vlldnt <vieilledent.adrien@gmail.com>
2025-02-04 10:15:27 +0100

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''creating a class named BaseGeometry'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Rectangle class sub from BaseGeometry'''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
