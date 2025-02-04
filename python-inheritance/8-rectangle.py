b6729f09c4351c01f89f22f96ffe00b5df476c39
vlldnt <vieilledent.adrien@gmail.com>
2025-02-04 09:44:38 +0100

    Task 8 not base on 7 but imported

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,22 +2,13 @@
 '''creating a class named BaseGeometry'''
 
 
-class BaseGeometry:
-    '''class BaseGemetry'''
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or type(value) is bool:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     '''Rectangle class sub from BaseGeometry'''
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
