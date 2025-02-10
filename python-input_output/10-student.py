b268c942c6e37092ad4b4d3f37882e83c3965070
vlldnt <vieilledent.adrien@gmail.com>
2025-02-10 15:28:05 +0100

    Task 10 - return key -value if in attrs

---
+++ python-input_output/10-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Class Students creation'''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(obj, str) for obj in attrs):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__
