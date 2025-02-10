100eb73752a1235358f5fd4d0a1baad79c3e0f49
vlldnt <vieilledent.adrien@gmail.com>
2025-02-10 16:40:34 +0100

    load() to loads()

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'''Returns an object represented by a JSON string)'''
+'''Returns an object represented by a JSON string'''
 
 
 import json
 
 
 def from_json_string(my_str):
-    '''Workng of JSON string, return'''
-    return json.load(my_str)
+    '''Returns an object represented by a JSON string'''
+    return json.loads(my_str)
