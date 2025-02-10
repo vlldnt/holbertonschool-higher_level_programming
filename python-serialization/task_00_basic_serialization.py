a33a250fa55f18bdcd95d334b0f96c20a03e4c73
vlldnt <vieilledent.adrien@gmail.com>
2025-02-10 16:54:51 +0100

    Pycodestyle and task 1

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-''''''
+'''Serialisze save and deserialize and load filename and data'''
 
 
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    '''Serialize and save to file data'''
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    '''Load and deserialize filename'''
+    with open(filename, "r") as file:
+        return json.load(file)
