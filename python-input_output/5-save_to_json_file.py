f9b6bd4486446f8105e5562271fded5070b3445b
vlldnt <vieilledent.adrien@gmail.com>
2025-02-10 11:53:52 +0100

    task 6 cratea object from json file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,3 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     with open(filename, "w") as file:
         json.dump(my_obj, file)
+        
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''creates an Object from a “JSON file”:'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, "r") as file:
+        return json.load(file)
