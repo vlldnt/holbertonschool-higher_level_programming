7e06c7f94e755940ad3261d12e1730eca88dd312
vlldnt <vieilledent.adrien@gmail.com>
2025-02-10 16:29:41 +0100

    READEME.md

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    '''read file and print'''
     with open(filename, "r", encoding="UTF-8") as file:
         for words in file:
             print("{}".format(words), end="")
--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,6 +3,8 @@
 
 
 class Student:
+    '''student class'''
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
