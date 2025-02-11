fbb6da6fbe10fda21b8e1ba6fd2f173f5d5ddfc6
vlldnt <vieilledent.adrien@gmail.com>
2025-02-11 11:13:38 +0100

    Task 2 serialize and deserialze with pickel module

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,3 +1,36 @@
 #!/usr/bin/python3
-''''''
+'''Pickle module for Serialize and deserialize'''
 
+
+import pickle
+
+
+class CustomObject:
+    '''Pickle module for Serialize and deserialize'''
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        '''Display dict key value'''
+        for key, value in self.__dict__.items():
+            print("{}: {}".format(key, value))
+
+    def serialize(self, filename):
+        '''Serialize using pickle module to a specified file'''
+        try:
+            with open(filename, "wb") as file:
+                # "wb" write/binary (Pickle method)
+                pickle.dump(self, file)
+        except Exception as e:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        '''Deserialize filename using pickle module'''
+        try:
+            with open(filename, "rb") as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            return None
