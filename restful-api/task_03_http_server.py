cba556bad23114ea32fd1663519e07629538fbf2
vlldnt <vieilledent.adrien@gmail.com>
2025-02-17 14:55:42 +0100

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,32 +2,31 @@
 '''http.server module use'''
 
 
-import http.server
-import socketserver
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 
-class Handler(http.server.BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     '''Subclass of http.server.BaseHTTPRequestHandler'''
 
     def do_GET(self):
         '''DO GET method'''
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
-            self.send_header("Content-type", "application/json; charset=utf-8")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Ok")
 
@@ -35,20 +34,25 @@ class Handler(http.server.BaseHTTPRequestHandler):
             info = {"version": "1.0",
                     "description": "A simple API built with http.server"}
             self.send_response(200)
-            self.send_header("Content-type", "application/json; charset=utf-8")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(info).encode('utf-8'))
 
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
-port = 8000
-
-
-with socketserver.TCPServer(("", port), Handler) as httpd:
-    print(f"Opening server at {port}")
+def run(server_class=HTTPServer, handler_class=Handler, port=8000):
+    """run the http server."""
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print("Starting server on port {}...".format(port))
     httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    """starts the programm."""
+    run()
