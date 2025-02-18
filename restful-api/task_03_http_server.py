f2667ebc2f41f1cccbadd77f0203f0335b4bf668
vlldnt <vieilledent.adrien@gmail.com>
2025-02-18 09:39:00 +0100

    restore 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,50 +1,58 @@
 #!/usr/bin/python3
-"""
-the programme will develop a simple API with flask
-"""
-
-from flask import Flask, jsonify, request
+'''http.server module use'''
 
 
-app = Flask(__name__)
-users = {}
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 
-@app.route("/")
-def home():
-    return "Welcome to the Flask API!"
+class Handler(BaseHTTPRequestHandler):
+    '''Subclass of http.server.BaseHTTPRequestHandler'''
+
+    def do_GET(self):
+        '''DO GET method'''
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            info = {"version": "1.0",
+                    "description": "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode('utf-8'))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
 
-@app.route("/data")
-def GU():
-    return jsonify(list(users.keys()))
+def run(server_class=HTTPServer, handler_class=Handler, port=8000):
+    """run the http server."""
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print("Starting server on port {}...".format(port))
+    httpd.serve_forever()
 
 
-@app.route('/status')
-def stat():
-    return "OK"
-
-
-@app.route('/users/<username>')
-def search_user(username):
-    user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({"error": "User not found"}), 404
-
-
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    data = request.get_json()
-    new = data.get('username')
-
-    if new:
-        users[new] = data
-        return jsonify({"message": "User added", "user": data}), 201
-    else:
-        return jsonify({"error": "Username is required"}), 400
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    """starts the programm."""
+    run()
