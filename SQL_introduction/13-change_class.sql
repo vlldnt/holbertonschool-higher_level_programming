e2077953374489de975149bb0a83429f6fca4a32
vlldnt <vieilledent.adrien@gmail.com>
2025-03-03 11:35:00 +0100

    Task 12 to 14

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Remove all records with a score <= 5
+DELETE from second_table WHERE score <= 5;
---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Display average score in the table
+SELECT AVG(score) AS average FROM second_table;
