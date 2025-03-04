0d3b38fb62731d4ba49a4af86c8a661fd0d68e96
vlldnt <vieilledent.adrien@gmail.com>
2025-03-04 15:23:11 +0100

    Task 1 with capital N starting

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-''''''
+'''Import all states from hbtn_0d_usa'''
 
 
 import MySQLdb
 import sys
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
