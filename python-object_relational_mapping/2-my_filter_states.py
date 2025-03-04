df6108751a60d96ad57534816703bcbd244d52fa
vlldnt <vieilledent.adrien@gmail.com>
2025-03-04 16:12:33 +0100

    Task2 with argument in the code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''Filter by name of state'''
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+        )
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states "
+                "WHERE name='{sys.argv[4]}' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
