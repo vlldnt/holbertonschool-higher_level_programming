f7328bba77d05c69a721fd8cb9dccd4a120b4c94
vlldnt <vieilledent.adrien@gmail.com>
2025-03-04 16:44:35 +0100

    Task 3 without SQL injectin possibilities

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+'''Safe filter to prevent from SQL injection'''
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+        )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name=%s ORDER BY id ASC", (sys.argv[4], )
+        )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
