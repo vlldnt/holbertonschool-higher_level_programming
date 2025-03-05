b0abca8d4c189c639aef1d65ec06c693a8b807f3
vlldnt <vieilledent.adrien@gmail.com>
2025-03-05 09:52:02 +0100

    Task 4 join states and cities

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+'''filter cities by state'''
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+        )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+        )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
