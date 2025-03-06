051835137c957d8b419c7db562d21f943689307c
vlldnt <vieilledent.adrien@gmail.com>
2025-03-06 10:34:55 +0100

    task 12 new mexico

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     session = Session()
 
     update = session.query(State).filter(State.id == 2).first()
-    update.name = "Mexico"
+    update.name = "New Mexico"
     session.commit()
 
     session.close()
