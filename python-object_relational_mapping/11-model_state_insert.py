f33cc4f512df2114c32d03e02d4edd5d09ea57a1
vlldnt <vieilledent.adrien@gmail.com>
2025-03-06 09:56:32 +0100

    Task 10

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+'''List all name with 'a' inside in he list order by state_id'''
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if not state:
+        print("not found")
+    else:
+        print("{}".format(state.id))
+
+    session.close()
