8635afc6cd43f8da23ca3593ee8e8dcec85bdd5c
vlldnt <vieilledent.adrien@gmail.com>
2025-03-19 11:04:25 +0100

    Task 00 with return

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -7,19 +7,23 @@ import os
 def generate_invitations(template, attendees):
     '''Generate invitations'''
     if not isinstance(template, str):
-        print('template must'
-                        f' be a string, got {type(template).__name__}')
+        print(f'template must be a string, got {type(template).__name__}')
+        return
     if not template:
         print('Template is empty, no output files generated.')
+        return
 
     if not isinstance(attendees, list) or \
         not all(isinstance(dict_attendee, dict)
                 for dict_attendee in attendees):
-        print('attendees must be a list'
-                        ' and with only dictionaries '
-                        f'got {type(attendees).__name__}')
+        print(
+            'attendees must be a list and '
+            'with only dictionaries got {}'.format(type(attendees).__name__)
+            )
+        return
     if not attendees:
         print(f'No data provided, no output files generated.')
+        return
 
     try:
         num = 1
@@ -35,8 +39,12 @@ def generate_invitations(template, attendees):
             if os.path.exists(f'output_{num}.txt'):
                 print(f'output_{num}.txt already exists, skipping writting')
                 continue
+
             with open(f'output_{num}.txt', 'w') as file:
                 file.write(mail)
+            print(f'output_{num}.txt created successfully.')
+
             num = num + 1
+
     except Exception as e:
-        print(f'An error occured while writting: {e}')
+        print(f'An error occured while writting output_{num}.txt: {e}')
