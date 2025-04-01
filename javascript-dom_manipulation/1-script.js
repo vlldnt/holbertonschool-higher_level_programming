4b60b8bc84cf20943f50235ee7d3181810f8f02e
vlldnt <vieilledent.adrien@gmail.com>
2025-04-01 13:59:37 +0200

    Task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+const click = document.querySelector('#red_header');
+const header = document.querySelector('header');
+click.addEventListener('click', function() {
+    header.style.color ='#FF0000'
+});
